e6cb83a1cd86f9ab7e2ba825ec819f95e7e7a89d
J Wang <janis.j.wang@gmail.com>
2019-09-19 01:52:56 +1000

    Updates

---
+++ precache-manifest.27cb88c216596444fd669e2b08e7fa2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6f4240628a0c51a5bca86fad3bf3c4d",
+    "url": "/simple-form-service/index.html"
+  },
+  {
+    "revision": "4c4062d07c0e1fa985cc",
+    "url": "/simple-form-service/static/css/main.f3b936fa.chunk.css"
+  },
+  {
+    "revision": "3c4af4186c4c1e0fdbfd",
+    "url": "/simple-form-service/static/js/2.fc6c6f78.chunk.js"
+  },
+  {
+    "revision": "4c4062d07c0e1fa985cc",
+    "url": "/simple-form-service/static/js/main.2207e0a5.chunk.js"
+  },
+  {
+    "revision": "8f8bbf3c4fe577badad0",
+    "url": "/simple-form-service/static/js/runtime~main.5531d02e.js"
+  }
+]);
